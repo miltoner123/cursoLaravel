197a24840361cfa6b8b5c8fc03a85f3877b168d8
miltoner123 <milton.condori.mbc@gmail.com>
2025-08-25 01:24:44 -0400

    funcionando con plantillas blade usuarios y condicionales

--- 02json/resources/views/greeting.blade.php
+++ 02json/resources/views/greeting.blade.php
@@ -8,5 +8,15 @@
 </head>
 <body>
     <h1>hola, {{ $name }}</h1>
+    <ul>
+        @foreach ($usuarios as $usuario)
+        <li>{{ $usuario['name'] }} - {{ implode(', ', $usuario['profesiones']?? []) }}</li>
+        @endforeach
+    </ul>
+    @if ($num1>$num2)
+        <p>num1 {{ $num1 }} es mayor</p>
+        @else
+        <p>num2 {{ $num2 }} es mayor</p>
+    @endif
 </body>
 </html>
--- 02json/routes/web.php
+++ 02json/routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('greeting', ['name'=>'Adimer']);
+    $usuarios=[
+        ['name'=>'Adimer',
+        'profesiones'=>['desarrollador']],
+    ['name'=>'Maria'
+    ],
+        ['name'=>'Adir',
+        'profesiones'=>['analista']
+],
+    ];
+   $num1=10;
+   $num2=20;
+    return view('greeting', ['name'=>'Adimer','usuarios'=>$usuarios, 'num1'=>$num1,'num2'=>$num2]);
 });
