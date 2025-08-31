46348f3598951dd240f4ee6042308f7188c139c4
miltoner123 <milton.condori.mbc@gmail.com>
2025-08-31 15:27:11 -0400

    Request Terminada

--- 03request/app/Http/Controllers/ProductoController.php
+++ 03request/app/Http/Controllers/ProductoController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreProductoRequest;
 
-class ProductoController extends Controller
-{
-    //
+class ProductoController extends Controller{
+    function store(StoreProductoRequest $request){
+    //     $validation = $request->validate([
+    //          'nombre' => 'required|string|max:15',
+    //         'precio' => 'required|numeric|min:0',
+
+    //   ]);
+    echo "soy guardar";
+    }
 }
--- 03request/routes/api.php
+++ 03request/routes/api.php
@@ -1,8 +1,10 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::post('productos',[ProductoController::class, 'store']);
